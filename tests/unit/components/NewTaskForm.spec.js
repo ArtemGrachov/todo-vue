de27013aa5979a8d8f2be715361ff6fdecd738d3
Artem Grachov <grachov2195@gmail.com>
2018-11-13 17:52:26 +0200

    NewTaskForm emitted value test

--- tests/unit/components/NewTaskForm.spec.js
+++ tests/unit/components/NewTaskForm.spec.js
@@ -17,4 +17,18 @@ describe('NewTaskForm.vue', () => {
     wrapper.vm.submit();
     expect(wrapper.emitted().submit).to.have.lengthOf(1);
   })
+
+  it('submit structure', () => {
+    const draft = {
+      title: 'Task #11',
+      description: 'Hello, World!'
+    }
+    wrapper.vm.form.title = draft.title;
+    wrapper.vm.form.description = draft.description;
+
+    wrapper.vm.submit();
+    const emitted = wrapper.emitted().submit[0][0];
+    expect(emitted.title).to.equal(draft.title);
+    expect(emitted.description).to.equal(draft.description);
+  })
 })
