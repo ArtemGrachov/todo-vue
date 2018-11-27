ba0746fd9136f622f4c339c7d4c00a27206d0e54
Artem Grachov <grachov2195@gmail.com>
2018-11-27 14:13:41 +0200

    Added unit tests for TaskItem component

---
+++ tests/unit/components/TaskItem.spec.js
@@ -0,0 +1,53 @@
+import {
+  expect
+} from 'chai'
+import {
+  shallowMount
+} from '@vue/test-utils'
+import TaskItem from '../../../src/components/TaskItem.vue';
+
+describe('TaskItem.vue', () => {
+  let wrapper;
+  const task = {
+    _id: '123123123',
+    title: 'Test item',
+    description: 'Item for unit test'
+  };
+
+  beforeEach(() => {
+    wrapper = shallowMount(TaskItem, {
+      propsData: {
+        task
+      }
+    });
+  })
+
+  it('emit "delete" event', () => {
+    wrapper.vm.deleteTask();
+    const emitted = wrapper.emitted().delete[0][0];
+    expect(emitted).to.equal(task._id);
+  })
+
+  it('emit "update" event', () => {
+    wrapper.vm.updateTask();
+    wrapper.vm.editForm.title = 'Updated title';
+    wrapper.vm.editForm.description = 'Description';
+    const emitted = wrapper.emitted().update[0];
+    expect(emitted).to.eql([
+      task._id,
+      wrapper.vm.editForm
+    ]);
+  })
+
+  it('turn on edit form', () => {
+    wrapper.vm.editTask();
+    expect(wrapper.vm.editMode).to.equal(true);
+  })
+
+  it('turn off edit form', () => {
+    wrapper.vm.editTask();
+    wrapper.vm.cancelEdit();
+    expect(wrapper.vm.editMode).to.equal(false);
+  })
+
+})
