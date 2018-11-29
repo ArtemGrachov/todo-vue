e38bb49f06f0cdb3b1a2a3b7e55fd3fa4a78627d
Artem Grachov <grachov2195@gmail.com>
2018-11-29 17:08:41 +0200

    New App component create task test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -24,5 +24,34 @@ describe('App.vue', () => {
     expect(wrapper.vm.newTaskForm).to.equal(formStateInverted);
   });
 
-  
+  it('get tasks list', () => {
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.tasks.length).to.equal(mockStore.state.tasks.length)
+    })
+  })
+
+  it('create task', () => {
+    const newTask = {
+      title: 'New task title',
+      description: 'New task description'
+    }
+
+    wrapper.vm.$nextTick()
+      .then(() => {
+        wrapper.vm.createTask(newTask);
+        return wrapper.vm.$nextTick()
+      })
+      .then(() => {
+        return wrapper.vm.$nextTick()
+      })
+      .then(() => {
+        const find = wrapper.vm.tasks.find(
+          task => {
+            return task.title === newTask.title &&
+              task.description === newTask.description
+          }
+        )
+        expect(find).to.be.ok
+      })
+  })
 })
--- tests/utils/mock-actions.js
+++ tests/utils/mock-actions.js
@@ -4,7 +4,33 @@ import apiConfig from '../../src/configs/api.config';
 import actionsFactory from '../../src/store/actions-factory';
 const tasksUrl = apiConfig.url + 'tasks';
 
+const tasks = [{
+  description: 'Mock task #1',
+  title: 'Task #1',
+  _id: '1'
+}, {
+  description: 'Mock task #2',
+  title: 'Task #2',
+  _id: '2'
+}];
+
 const mock = new MockAdapter(axios);
-mock.onGet(tasksUrl).reply(200, []);
+mock.onGet(tasksUrl).reply(200, [...tasks]);
+mock.onPost(tasksUrl).reply(req => {
+  return [
+    200,
+    (Object.assign({
+      _id: '3'
+    }, JSON.parse(req.data)))
+  ]
+});
+mock.onPut(tasksUrl + '/:id').reply(req => {
+  return [
+    200,
+    (Object.assign({},
+      tasks.find(task => task._id === req.params.id),
+      req.data))
+  ]
+});
 
 export default actionsFactory(axios);
