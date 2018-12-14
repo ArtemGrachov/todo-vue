293c15434fdfedfc3157112483b2831995940028
Artem Grachov <grachov2195@gmail.com>
2018-12-14 17:19:32 +0200

    Vuex mutations unit testing

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -10,6 +10,47 @@ describe('Store mutations', () => {
 
   beforeEach(() => {
     state = stateFactory();
+  });
+
+  it('set tasks', () => {
+    const data = mockDataFactory();
+    mutations.SET_TASKS(state, data);
+    expect(state.tasks).to.deep.equal(data);
+  });
+
+  it('add task', () => {
+    const taskToAdd = mockDataFactory()[0];
+    mutations.ADD_TASK(state, taskToAdd);
+    expect(state.tasks).to.include(taskToAdd);
   })
 
-})
+  it('update task', () => {
+    const data = mockDataFactory();
+    mutations.SET_TASKS(state, data);
+
+    const taskToUpdate = state.tasks[0],
+      updateData = {
+        _id: taskToUpdate._id,
+        title: 'Updated title',
+        description: 'Updated description'
+      };
+
+    mutations.UPDATE_TASK(state, updateData);
+
+    const updatedTask = state.tasks.find(task => task._id === taskToUpdate._id);
+
+    expect(updatedTask.title).to.equal(updateData.title);
+    expect(updatedTask.description).to.equal(updateData.description);
+  })
+
+  it('delete task', () => {
+    const data = mockDataFactory();
+    mutations.SET_TASKS(state, data);
+
+    const taskToDeleteId = state.tasks[0]._id;
+
+    mutations.DELETE_TASK(state, taskToDeleteId);
+
+    expect(state.tasks.find(task => task._id === taskToDeleteId)).not.to.be.ok;
+  })
+});
